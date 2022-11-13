5e785f9eaefff15bf4984d4904ee9a0c4110cf21
Qusz <de.gorchakov@gmail.com>
2022-11-13 17:24:54 +0300

    reverts to relative path for pickIcon

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -129,29 +129,29 @@ export class Utilities {
       default:
         return 'ERROR: Incorrect wind direction';
     }
-  }assets
+  }
 
   pickIcon(weatherStatus) {
     switch(weatherStatus) {
       case 'Clear sky':
       case 'Mainly clear':
-        return 'assets/img/weather_icons/animated/clear-day.svg';
+        return './assets/img/weather_icons/animated/clear-day.svg';
       case 'Partly cloudy':
-        return 'assets/img/weather_icons/animated/partly-cloudy-day.svg';
+        return './assets/img/weather_icons/animated/partly-cloudy-day.svg';
       case 'Overcast':
-        return 'assets/img/weather_icons/animated/overcast-day.svg';
+        return './assets/img/weather_icons/animated/overcast-day.svg';
       case 'Fog':
-        return 'assets/img/weather_icons/animated/fog.svg';
+        return './assets/img/weather_icons/animated/fog.svg';
       case 'Drizzle':
-        return 'assets/img/weather_icons/animated/drizzle.svg';
+        return './assets/img/weather_icons/animated/drizzle.svg';
       case 'Rain':
       case 'Showers':
-        return 'assets/img/weather_icons/animated/rain.svg';
+        return './assets/img/weather_icons/animated/rain.svg';
       case 'Freezing rain':
-        return 'assets/img/weather_icons/animated/sleet.svg';
+        return './assets/img/weather_icons/animated/sleet.svg';
       case 'Snowfall':
       case 'Snow showers':
-        return 'assets/img/weather_icons/animated/snow.svg';
+        return './assets/img/weather_icons/animated/snow.svg';
       default:
         return ('ERROR: Can\'t load weather icon');
     }
